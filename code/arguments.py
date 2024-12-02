15c90d84e5dc2a4b6653206d89e728a0389d5269
lycge20923 <lawrence0999@gmail.com>
2024-12-02 11:55:47 +0800

    feat: adjust the structure of declaring arguments

---
+++ code/arguments.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+@dataclass
+class LLMArguments:
+    model_name: Optional[str] = field(
+        default="Qwen/Qwen2.5-7B-Instruct",
+        metadata={"help":"The name of llm."})
+    device: Optional[str] = field(
+        default='cuda:0'
+    )
+    use_8bit: Optional[bool] = field(
+        default=False,
+        metadata={"help":"Whether use 8 bits or not."}
+    )
+    use_wandb: Optional[bool] = field(
+        default=False,
+        metadata={"help":"Whether use wandb to track or not."}
+    )
+
+@dataclass
+class RAGArguments:
+    embedding_model: Optional[str] = field(
+        default="BAAI/bge-base-en-v1.5",
+        metadata={"help":"The model name of embedding model"}
+    )
+    seed: Optional[int] = field(
+        default=42
+    )
+    top_k: Optional[int] = field(
+        default=16
+    )
+    order: Optional[str] = field(
+        default="similar_at_top"
+    )
+
+@dataclass
+class ClassificationArguments:
+    max_tokens: Optional[int] = field(
+        default=16
+    )
+
+@dataclass
+class SQLGenerationArguments:
+    max_tokens: Optional[int] = field(
+        default=512
+    )
+
+if __name__ == "__main__":
+    test_LLMArguments = LLMArguments()
+    test_RAGArguments = RAGArguments()
+    combined_args = {**test_LLMArguments.__dict__, **test_RAGArguments.__dict__}
+    print(combined_args)
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from base import Agent
 from execution_pipeline import main
+from argparse import ArgumentParser
+from execution_pipeline import main
+from datetime import datetime
+from types import SimpleNamespace
 
+from code.arguments import LLMArguments, RAGArguments, ClassificationArguments, SQLGenerationArguments
 class ClassificationAgent(Agent):
     """
     An agent that classifies text into one of the labels in the given label set.
@@ -89,25 +94,50 @@ class SQLGenerationAgent(Agent):
         raise NotImplementedError
         
 if __name__ == "__main__":
-    from argparse import ArgumentParser
-    from execution_pipeline import main
-
+    # from argparse import ArgumentParser
+    # from execution_pipeline import main
+    
+    # declare required arguments for the main.py
     parser = ArgumentParser()
     parser.add_argument('--bench_name', type=str, required=True)
+    parser.add_argument('--output_path', type=str, required=True)
     args = parser.parse_args()
-
+    
+    # pass for function main
+    bench_cfg = {
+        'bench_name': args.bench_name, 
+        'output_path': args.output_path
+    }
+    
+    # pass for the model
     if args.bench_name.startswith("classification"):
         agent_name = ClassificationAgent
+        models_args = SimpleNamespace(**LLMArguments().__dict__, **ClassificationArguments().__dict__)
     elif args.bench_name.startswith("sql_generation"):
         agent_name = SQLGenerationAgent
+        models_args = SimpleNamespace(**LLMArguments().__dict__, **SQLGenerationArguments().__dict__)
     else:
         raise ValueError(f"Invalid benchmark name: {args.bench_name}")
-
-    bench_cfg = {
-        'bench_name': args.bench_name
-    }
+    
+    RAG_args = RAGArguments()
+    
+    implement_time = datetime.now()
+    implement_time = implement_time.strftime("%Y%m%d-%H%M%S")
+    
     config = {
-        # TODO: specify your configs for the agent here
+        'model_name': models_args.model_name,
+        'exp_name': f"{implement_time}_{bench_cfg['bench_name']}_{bench_cfg['output_path']}",
+        'bench_name': bench_cfg['bench_name'],
+        'max_tokens': models_args.max_tokens,
+        'do_sample': False,
+        'device': models_args.device,
+        'use_8bit': models_args.use_8bit,
+        'rag': {
+            'embedding_model': RAG_args.embedding_model,
+            'seed': RAG_args.seed,
+            "top_k": RAG_args.top_k,
+            "order": RAG_args.order
+        }
     }
     agent = agent_name(config)
     main(agent, bench_cfg)
